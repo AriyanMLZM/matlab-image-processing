2a3b29788d6338d60a66066107e2ab1ce42d2254
AriyanMlzm <ryanmolazem@gmail.com>
2024-06-18 23:35:47 +0330

    add laplacian transform

--- project.m
+++ project.m
@@ -1,3 +1,14 @@
 clc;
 clear;
 close all;
+
+% a) get image
+image = imread("input.jpg");
+image = imresize(image, [512 512]);
+image = im2gray(image);
+
+% b) implement laplacian
+lap = locallapfilt(image, 0.5, 0.5);
+
+% c) add laplacian to original
+added_image = imadd(image, lap);
